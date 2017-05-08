a723398cfdd506c163ccecb9159bdc3de94bd47c
kevin <277358492@qq,com>
2017-05-08 16:39:43 +0800

    iview升级为ivew2,vue升级为vue2

---
+++ src/main/webapp/src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue';
+import iView from 'iview';
+import VueRouter from 'vue-router';
+import Routers from './router';
+import Util from './libs/util';
+import App from './app.vue';
+import 'iview/dist/styles/iview.css';
+import axios from 'axios'
+
+Vue.use(VueRouter);
+Vue.use(iView);
+
+// 将axios插件对象附加到vue的原型上
+Vue.prototype.$http = axios;
+
+// 路由配置
+const RouterConfig = {
+    //mode: 'history',  // 默认是hash
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    iView.LoadingBar.start();
+    Util.title(to.meta.title);
+    next();
+});
+
+router.afterEach((to, from, next) => {
+    iView.LoadingBar.finish();
+    window.scrollTo(0, 0);
+});
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
